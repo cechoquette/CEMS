89c16bd01b72621b61b959e633bc7aeeae44f68d
Luke Climenhage <luke.climenhage@gmail.com>
2022-03-24 14:59:30 -0400

    Adding UI Controllers

---
+++ CEMS/src/controllers/ForgotPasswordUIController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ForgotPasswordUIController {
+
+    @FXML
+    private Button btnForgotBack;
+
+    @FXML
+    private Button btnForgotRecover;
+
+    @FXML
+    void btnForgotBackClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnForgotRecoverClicked(ActionEvent event) {
+
+    }
+
+}
+
