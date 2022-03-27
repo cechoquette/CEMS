9022ee40ff68037636f23b4f4c3a92530f556775
Catherine Choquette <cchoquette.1@gmail.com>
2022-03-26 21:49:10 -0400

    Updates to main as well as few minor changes. Created Event classes. Updated Controller.java.

--- CEMS/src/application/Main.java
+++ CEMS/src/application/Main.java
@@ -7,15 +7,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class Main extends Application {
+	//do not delete, global User for session. Initialize in Start.
+	public User currentUser;
+
+
+
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+
+			//Create Database (if first time running app)
+
+			//Create Clubs if Database empty
+				//Maybe make a method for this in Club class called createFirstClubs() and send to Database. Or RequestType.
+
+			//Retrieve and populate class static IDs from Database
+				//Club, User, Event, Expenditure, EventBudget, ClubBudget
+				//Ex
+				Event.setCountForIds(25);
+				//User.setCountForIds();//this one is different, UserId is student number. So no static count required??
+				Expenditure.setCOUNT_FOR_IDS(25);
+	//			EventBudget
+
+			//Initialize User currentUser during login
+
 
 			//for Paths: use absolute paths (leading / ) and do note use .. , the resulting .jar executable will not function.
 			Parent root;
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ClubBudgetSubmissionForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateEventForm.fxml"));
-//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateUserForm.fxml"));
+			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateUserForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/DeleteUserForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/EventBudgetSubmissionForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ExpenditureSubmissionForm.fxml"));
@@ -23,7 +45,7 @@ public class Main extends Application {
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/HomePage.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/LoginPage.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ProfilePage.fxml"));
-			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ReportRequestForm.fxml"));
+//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ReportRequestForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/RequestAccessForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/UpdateUserForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/UserManagement.fxml"));
