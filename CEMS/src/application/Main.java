888c3e43449d733a9b2cd4b6009390997ceedb06
Catherine Choquette <cchoquette.1@gmail.com>
2022-03-28 14:46:50 -0400

    committing main to merge

--- CEMS/src/application/Main.java
+++ CEMS/src/application/Main.java
@@ -36,10 +36,10 @@ public class Main extends Application {
 			//for Paths: use absolute paths (leading / ) and do note use .. , the resulting .jar executable will not function.
 			Parent root;
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ClubBudgetSubmissionForm.fxml"));
-//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateEventForm.fxml"));
+//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateClubEventForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/CreateUserForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/DeleteUserForm.fxml"));
-//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/EventBudgetSubmissionForm.fxml"));
+//			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ClubEventBudgetSubmissionForm.fxml"));
 			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ExpenditureSubmissionForm.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/ForgotPassword.fxml"));
 //			root = FXMLLoader.load(getClass().getResource("/CEMS/src/resources/fxml/HomePage.fxml"));
