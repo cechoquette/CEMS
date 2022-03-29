6987b7a45a1b78e8e102035ad4ce545f26d49f2b
zgweks <92344996+zgwek@users.noreply.github.com>
2022-03-29 15:14:19 -0400

    // add hibernate

--- module-info.java
+++ module-info.java
@@ -9,6 +9,6 @@ module CEMS {
     requires org.hibernate.orm.core;
 
 
-    opens CEMS.src.application to javafx.graphics, javafx.fxml;
+    opens CEMS.src.application to javafx.graphics, javafx.fxml,org.hibernate.orm.core;
     opens CEMS.src.controllers to javafx.fxml, javafx.graphics;
 }
