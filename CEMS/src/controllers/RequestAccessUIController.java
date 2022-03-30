21c063a2cb67a06024eef1ae768f2836a9ae4902
Luke Climenhage <luke.climenhage@gmail.com>
2022-03-30 13:47:43 -0400

    Added navigation to RequestAccessForm.fxml and fixed a missing method

--- CEMS/src/controllers/RequestAccessUIController.java
+++ CEMS/src/controllers/RequestAccessUIController.java
@@ -1,11 +1,15 @@
 package CEMS.src.controllers;
 
+import CEMS.src.application.ViewBuilder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import static CEMS.src.application.Main.defaultPane;
 
 public class RequestAccessUIController {
 
@@ -41,17 +45,36 @@ public class RequestAccessUIController {
 
     @FXML
     void btnRequestAccessBackClicked(ActionEvent event) {
-
+        // Return the user to the login page
+        Pane mainScreen = ViewBuilder.newScreen("LoginPage");
+        defaultPane.setCenter(mainScreen);
     }
 
     @FXML
     void btnRequestAccessCancelClicked(ActionEvent event) {
+        // TODO: Mention any abort or cancel method or tracking needed
 
+        // Return the user to the login page
+        Pane mainScreen = ViewBuilder.newScreen("LoginPage");
+        defaultPane.setCenter(mainScreen);
     }
 
     @FXML
     void btnRequestAccessSubmitClicked(ActionEvent event) {
+        // TODO: Validation of fields and error checking
 
+        // TODO: Submit the info to wherever it needs to go
+
+        // Return the user to the login page
+        Pane mainScreen = ViewBuilder.newScreen("LoginPage");
+        defaultPane.setCenter(mainScreen);
+    }
+
+    @FXML
+    void btnRequestAccessClearClicked(ActionEvent event) {
+        //Reload the screen
+        Pane mainScreen = ViewBuilder.newScreen("RequestAccessForm");
+        defaultPane.setCenter(mainScreen);
     }
 
 }
