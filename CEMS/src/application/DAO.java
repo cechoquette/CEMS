e7343cfd3010e1955ed802eff03436bbbcc811d5
zgweks <92344996+zgwek@users.noreply.github.com>
2022-03-31 22:47:13 -0400

    // delete tests

--- CEMS/src/application/DAO.java
+++ CEMS/src/application/DAO.java
@@ -512,106 +512,10 @@ public class DAO {
     }
 */
 
-   /*
-
-    //// Add
-    public void addUser(User user) {
-        Transaction transaction = null;
-        Session session = newSession.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-            System.out.println("Save Successful");
-        } catch (RuntimeException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-    }
-
-    public void addClub(Club club) {
-        Transaction transaction = null;
-        Session session = newSession.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            session.save(club);
-            session.getTransaction().commit();
-            System.out.println("Save Successful");
-        } catch (RuntimeException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-    }
-
-    public void addExpenditure(Expenditure expenditure) {
-        Transaction transaction = null;
-        Session session = newSession.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            session.save(expenditure);
-            session.getTransaction().commit();
-            System.out.println("Save Successful");
-        } catch (RuntimeException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-    }
-
-    //// GET
-
-
-    //// Lists
-
-    public List<Club> getClubs() {
-        List<Club> clubs = new ArrayList<Club>();
-        Transaction transaction = null;
-        Session session = newSession.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            clubs = session.createQuery("from Club" ).list();
-
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return clubs;
-    }
-
-    public List<Expenditure> getExpenditures() {
-        List<Expenditure> expenditures = new ArrayList<Expenditure>();
-        Transaction transaction = null;
-        Session session = newSession.getSessionFactory().openSession();
-        try {
-            transaction = session.beginTransaction();
-            expenditures
-                    = session.createQuery("from Expenditure" ).list();
-
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return expenditures;
-    }
 
 
 
-/* User Test
+/*
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
         DAO dao = new DAO();
         Club club1 = new Club("cs club","we enjoy cs");
